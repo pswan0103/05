1dfbf6a8749d45a0dac017e331dd1333fdec7789
pswan0103 <22zch@naver.com>
2022-10-05 18:26:39 +0900

    6

--- main.c
+++ main.c
@@ -4,22 +4,32 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x, y, result;
-	char op;
+	int x;
+	int trials=0;
+	int answer=71;
 	
-	printf("enter the calculation: ");
-	scanf("%d %c %d", &x, &op, &y);
-	
-	if (op=='+')
-		result = x+y;
-	else if (op=='-')
-		result = x-y;
-	else if (op=='*')
-		result = x*y;
-	else if (op=='/')
-		result = x/y;
-	
-	printf("= %d", result);
+	do
+	{
+		printf("Guess a number: ");
+		scanf("%d", &x);
+		
+		if (x<answer)
+		{
+			printf("low!\n");
+			trials++;
+		}
+		else if (x>answer)
+		{
+			printf("high!\n");
+			trials++;
+		}
+		else
+		{
+			trials++;
+			printf("Congratulation! trials: %d", trials);
+		}
+	}
+	while (x!=answer);
 	
 	return 0;
 }
